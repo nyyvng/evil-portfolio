18dbbde236495f3dd4799a3949645626e4612572
Naylia <nylia.vang1@gmail.com>
2026-05-11 18:10:55 -0700

    issue resolved

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // no js? what
 
-const menuBtn = document.getElementById("menu-btn");
-const dropdown = document.getElementById("dropdown-menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("menu-btn");
+    const dropdown = document.getElementById("dropdown-menu");
 
-menuBtn.addEventListener("click", () => {
-    dropdown.classList.toggle("show-menu");
+    menuBtn.addEventListener("click", () => {
+        dropdown.classList.toggle("show-menu");
+    });
 });
